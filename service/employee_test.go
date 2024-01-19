33960e3735fd0729c9fd9682c1eabd57e15e472b
Najib Jodiansyah <najibjodiansyah11@gmail.com>
2024-01-19 19:45:28 +0700

    tidy up service test

--- service/employee_test.go
+++ service/employee_test.go
@@ -2,6 +2,7 @@ package service_test
 
 import (
 	"context"
+	"errors"
 	"testing"
 
 	"github.com/najibjodiansyah/mekari-employee/model/domain"
@@ -36,10 +37,98 @@ func TestEmployeeService(t *testing.T) {
 				}
 
 				mockEmployeeRepo.EXPECT().SelectAll(ctx).Return(employee, nil)
-				_, err := employeeService.Find(ctx)
+				e, err := employeeService.Find(ctx)
 				So(err, ShouldBeNil)
+				So(e, ShouldNotBeNil)
+			})
+			Convey("Test Get Employees Failed", func() {
+				mockEmployeeRepo.EXPECT().SelectAll(ctx).Return(nil, errors.New("error"))
+				e, err := employeeService.Find(ctx)
+				So(err, ShouldNotBeNil)
+				So(e, ShouldBeNil)
+			})
+		})
+		Convey("Test Get Employee By Id", func() {
+			Convey("Test Get Employee By Id Success", func() {
+				hireDate := "2020-01-01 15:00:00"
+				employee := &domain.Employee{
+					Id:        1,
+					FirstName: "Najib",
+					LastName:  "Jodiansyah",
+					Email:     "najib@test.com",
+					HireDate:  &hireDate,
+				}
+
+				mockEmployeeRepo.EXPECT().SelectById(ctx, 1).Return(employee, nil)
+				e, err := employeeService.FindById(ctx, 1)
+				So(err, ShouldBeNil)
+				So(e, ShouldNotBeNil)
+			})
+			Convey("Test Get Employee By Id Failed", func() {
+				mockEmployeeRepo.EXPECT().SelectById(ctx, 1).Return(nil, errors.New("error"))
+				e, err := employeeService.FindById(ctx, 1)
+				So(err, ShouldNotBeNil)
+				So(e, ShouldBeNil)
+			})
+		})
+		Convey("Test Create Employee", func() {
+			Convey("Test Create Employee Success", func() {
+				hireDate := "2020-01-01 15:00:00"
+				employee := &domain.Employee{
+					Id:        1,
+					FirstName: "Najib",
+					LastName:  "Jodiansyah",
+					Email:     "najib@test.com",
+					HireDate:  &hireDate,
+				}
+				mockEmployeeRepo.EXPECT().Insert(ctx, employee).Return(nil)
+				err := employeeService.Create(ctx, employee)
+				So(err, ShouldBeNil)
+			})
+			Convey("Test Create Employee Failed", func() {
+				hireDate := "2020-01-01 15:00:00"
+				employee := &domain.Employee{
+					Id:        1,
+					FirstName: "Najib",
+					LastName:  "Jodiansyah",
+					Email:     "najib@test.com",
+					HireDate:  &hireDate,
+				}
+				mockEmployeeRepo.EXPECT().Insert(ctx, employee).Return(errors.New("error"))
+				err := employeeService.Create(ctx, employee)
+				So(err, ShouldNotBeNil)
+			})
+			Convey("Test Create Employee Failed Parse Datetime", func() {
+				hireDate := "2020-01-01 15:00:00Z"
+				employee := &domain.Employee{
+					Id:        1,
+					FirstName: "Najib",
+					LastName:  "Jodiansyah",
+					Email:     "najib@test.com",
+					HireDate:  &hireDate,
+				}
+				err := employeeService.Create(ctx, employee)
+				So(err.Error(), ShouldEndWith, "HireDate format must be RFC3339")
+			})
+		})
+		Convey("Test Delete Employee", func() {
+			Convey("Test Delete Employee Not Found", func() {
+				mockEmployeeRepo.EXPECT().SelectById(ctx, 1).Return(nil, errors.New("error"))
+				err := employeeService.Delete(ctx, 1)
+				So(err, ShouldNotBeNil)
+			})
+			Convey("Test Delete Employee Success", func() {
+				mockEmployeeRepo.EXPECT().SelectById(ctx, 1).Return(&domain.Employee{}, nil)
+				mockEmployeeRepo.EXPECT().Delete(ctx, 1).Return(nil)
+				err := employeeService.Delete(ctx, 1)
+				So(err, ShouldBeNil)
+			})
+			Convey("Test Delete Employee Failed", func() {
+				mockEmployeeRepo.EXPECT().SelectById(ctx, 1).Return(&domain.Employee{}, nil)
+				mockEmployeeRepo.EXPECT().Delete(ctx, 1).Return(errors.New("error"))
+				err := employeeService.Delete(ctx, 1)
+				So(err, ShouldNotBeNil)
 			})
 		})
 	})
-
 }
